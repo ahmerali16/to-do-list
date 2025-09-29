4510069f12952c9172f5b66dc525ad42c6cd7602
ahmerali16 <ahmarbhutto0@gmail.com>
2025-09-30 04:22:17 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("write something please");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";  // × symbol
+        li.appendChild(span);
+
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
